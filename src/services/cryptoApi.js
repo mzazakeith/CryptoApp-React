79fe50e65d9de68eac8ec644f22b8906cecb8a69
mzazakeith <mzazakeith@gmail.com>
2022-01-24 20:31:13 +0300

    api for getting coins created

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query";
+
+const cryptoApiHeaders ={
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'f3d92f4cd9msh73b3eb298292b51p19c125jsneb70c7ac7f5f'
+}
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({
+    url,
+    headers: cryptoApiHeaders
+})
+
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCoins: builder.query({
+            query: () => createRequest("/coins")
+        })
+    })
+})
