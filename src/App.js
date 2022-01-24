1d7a344ad886834fc8c68ec229b5a6a15adad5fb
mzazakeith <mzazakeith@gmail.com>
2022-01-24 16:26:59 +0300

    added simple navbar componeent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import {Switch, Route, Link} from 'react-router-dom';
+import {Layout, Typography, Space} from "antd";
+
+import {Navbar} from './components';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className="navbar">
+        <Navbar/>
+      </div>
+      <div className="main">
 
+      </div>
+      <div className="footer">
+
+      </div>
     </div>
   );
 }
