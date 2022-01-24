3e075e46ca1728b54d7cf50225688f3c934cd65c
mzazakeith <mzazakeith@gmail.com>
2022-01-24 16:56:56 +0300

    simple components added and exported

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {Switch, Route, Link} from 'react-router-dom';
 import {Layout, Typography, Space} from "antd";
 
-import {Navbar} from './components';
+import {Navbar,Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News} from './components';
 import './App.css';
 
 function App() {
@@ -11,7 +11,17 @@ function App() {
         <Navbar/>
       </div>
       <div className="main">
-
+        <Layout>
+            <div className="routes">
+                <Switch>
+                    <Route exact path="/"><Homepage/></Route>
+                    <Route exact path="/exchanges"><Exchanges/></Route>
+                    <Route exact path="/cryptocurrencies"><Cryptocurrencies/></Route>
+                    <Route exact path="/crypto/:coinId"><CryptoDetails/></Route>
+                    <Route exact path="/news"><News/></Route>
+                </Switch>
+            </div>
+        </Layout>
       </div>
       <div className="footer">
 
