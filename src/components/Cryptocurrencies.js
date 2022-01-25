97e5e2fccad7d5c9888df881e9b0c7c4788ea4ae
mzazakeith <mzazakeith@gmail.com>
2022-01-25 11:29:13 +0300

    getting coins done

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
+import millify from "millify";
+import {Link} from 'react-router-dom';
+import {Card, Input, Col, Row} from 'antd';
+
+import {useGetCoinsQuery} from "../services/cryptoApi";
 
 const Cryptocurrencies = () => {
+    const {data:cryptosList, isFetching} = useGetCoinsQuery();
+    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+    console.log(cryptos);
+
     return(
         <div>
             Cryptocurrencies
