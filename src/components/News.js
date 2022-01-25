e306edfea9ca6159ff9869c28184d28407d504a9
mzazakeith <mzazakeith@gmail.com>
2022-01-25 14:11:22 +0300

    enabled filtering of news based on a specific coin

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,9 @@
-import React from "react";
-import {Select, Typography, Row, Col, Avatar, Card} from "antd"
+import React, {useState} from "react";
+import {Select, Typography, Row, Col, Avatar, Card, Input} from "antd"
 import moment from "moment";
 
 import {useGetNewsQuery} from "../services/newsApi";
+import {useGetCoinsQuery} from "../services/cryptoApi";
 
 const {Text, Title} = Typography;
 const {Option} =Select;
@@ -10,18 +11,38 @@ const {Option} =Select;
 const demoImage = "https://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg";
 
 const News = ({simplified}) => {
-    const {data:cryptoNews} = useGetNewsQuery({newsCategory:"cryptocurrency", count: simplified ? 6 : 12});
+    const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
+    const {data:cryptoNews} = useGetNewsQuery({newsCategory, count: simplified ? 6 : 12});
+    const {data} = useGetCoinsQuery(100);
 
     if(!cryptoNews?.value) return "Loading"
     return(
         <Row gutter={[ 24, 24 ]}>
+            {!simplified && (
+                <Col span={24}>
+                    <Select
+                        showSearch
+                        className="select-news"
+                        placeholder="Select a cryptocurrency"
+                        optionFilterProp="children"
+                        onChange={(value) => setNewsCategory(value)}
+                        filterOption={(input, option)=>
+                            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                    >
+                        <Option value="Cryptocurrency"> Cryptocurrency</Option>
+                        {data?.data?.coins.map((coin) =>
+                            <Option value={coin.name}>{coin.name}</Option>)
+                        }
+                    </Select>
+                </Col>
+            )}
             {cryptoNews.value.map((news, i) => (
                 <Col xs={24} sm={12} lg={8} key={i}>
                     <Card hoverable className="news-card">
                         <a href={news.url} target="_blank" rel="noreferrer">
                             <div className="news-image-container">
                                 <Title className="news-title" level={4}>{news.name}</Title>
-                                <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news"/>
+                                <img style={{maxWidth:'200px', maxHeight:'100px'}} src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news"/>
                             </div>
                             <p>
                                 {news.description.length > 100
